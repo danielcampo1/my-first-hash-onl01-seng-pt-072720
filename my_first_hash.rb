16873e8711fb14bc9885927eeb309ed48d324e38
daniel campo <campo_daniel94@yahoo.com>
2020-08-08 11:57:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,9 +20,11 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+end
 
 shipping_manifest["oil paintings"]
-end
+
+
 
 def adding
   shipping_manifest = {
@@ -39,6 +41,7 @@ shipping_manifest["muskets"] = "2"
 shipping_manifest["gun powder"] = "4"
 
   # return the shipping_manifest hash below
+  
 shipping_manifest
 
 end
